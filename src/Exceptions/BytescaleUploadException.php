3379d7e99b3ba30d82708a2571a648f375a60f88
Usman Zahid <developerusman@yahoo.com>
2025-09-07 22:19:39 +0500

    Minor changes

--- src/Exceptions/BytescaleUploadException.php
+++ src/Exceptions/BytescaleUploadException.php
@@ -4,6 +4,8 @@ namespace UsmanZahid\Bytescale\Exceptions;
 
 use Exception;
 
+// Responses like the n8n sdk might be better.
+// Or maybe we can have custom exceptions for the SDK.
 class BytescaleUploadException extends Exception {
     public function __construct(
         string     $message = "An error occurred during Bytescale upload.",
