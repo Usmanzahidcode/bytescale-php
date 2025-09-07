c3b95c0c524748e061d074d567d1fb88f1e64f41
Usman Zahid <developerusman@yahoo.com>
2025-09-07 22:19:50 +0500

    Minor changes

--- src/Exceptions/GeneralBytescaleException.php
+++ src/Exceptions/GeneralBytescaleException.php
@@ -6,6 +6,8 @@ namespace UsmanZahid\Bytescale\Exceptions;
 
 use Exception;
 
+// Responses like the n8n sdk might be better.
+// Or maybe we can have custom exceptions for the SDK.
 class GeneralBytescaleException extends Exception {
     public function __construct(
         string     $message = "A general Bytescale error has occurred.",
